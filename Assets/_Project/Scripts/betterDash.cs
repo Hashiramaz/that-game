b3cb923d4ecb8948edab3449e8d7e01ae7e28fb5
Infran <Infran@github.com>
2020-04-29 03:17:22 -0400

    Update Dash

--- Assets/_Project/Scripts/betterDash.cs
+++ Assets/_Project/Scripts/betterDash.cs
@@ -11,90 +11,73 @@ public class betterDash : MonoBehaviour
     private int directionH;
     private int directionV;
     private float moveInput;
+    public bool isOnDash;
+    public bool canUseDash
+    {
+        get
+        {
+            return !isOnDash;
+        }
+    }
+    public Vector2 dashDirection;
+    public float currentDashTime;
+    private bool buttonDashPressed;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         dashTime = startDashTime;
     }
+    public void UpdateDashInput()
+    {
+        if (Input.GetButtonDown("Dash") && !buttonDashPressed)
+        {
+            buttonDashPressed = true;
+            TryUseDash();
+        }
+        if (Input.GetButtonUp("Dash") && buttonDashPressed)
+        {
+            buttonDashPressed = false;
+        }
+    }
+    public void TryUseDash()
+    {
+        if (canUseDash)
+        {
+            StartDash();
+        }
+    }
+    public void StartDash()
+    {
+        SetDashDirection();
+        isOnDash = true;
+    }
+    public void SetDashDirection()
+    {
+        dashDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+    public void UpdateDashMoviment()
+    {
+        if (isOnDash)
+        {
+            if (dashTime > 0)
+            {
+                dashTime -= Time.deltaTime;
+                rb.velocity = dashDirection * dashSpeed;
+            }
+            else
+            {
+                dashTime = startDashTime;
+                rb.velocity = Vector2.zero;
+                isOnDash = false;
+            }
+        }
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (directionH == 0)
-        {
-            if (Input.GetButtonDown("Dash"))
-            {
-                moveInput = Input.GetAxis("Horizontal");
-                 if (moveInput < 0)
-                {
-                    directionH = 1;
-                }
-                else if (moveInput > 0)
-                {
-                    directionH = 2;
-                }
-            }
-        }
-        else
-        {
-            if (dashTime <= 0)
-            {
-                directionH = 0;
-                dashTime = startDashTime;
-                rb.velocity = Vector2.zero;
-            }
-            else
-            {
-                dashTime -= Time.deltaTime;
-
-                if (directionH == 1)
-                {
-                    rb.velocity = Vector2.left * dashSpeed;
-                }
-                else if (directionH == 2)
-                {
-                    rb.velocity = Vector2.right * dashSpeed;
-                }
-            }
-        }
-
-        if (directionV == 0)
-        {
-            if (Input.GetButtonDown("Dash"))
-            {
-                moveInput = Input.GetAxis("Vertical");
-                 if (moveInput < 0)
-                {
-                    directionV = 1;
-                }
-                else if (moveInput > 0)
-                {
-                    directionV = 2;
-                }
-            }
-        }
-        else
-        {
-            if (dashTime <= 0)
-            {
-                directionV = 0;
-                dashTime = startDashTime;
-                rb.velocity = Vector2.zero;
-            }
-            else
-            {
-                dashTime -= Time.deltaTime;
-
-                if (directionV == 1)
-                {
-                    rb.velocity = Vector2.down * dashSpeed;
-                }
-                else if (directionV == 2)
-                {
-                    rb.velocity = Vector2.up * dashSpeed;
-                }
-            }
-        }
+        UpdateDashMoviment();
+        UpdateDashInput();
     }
 }
